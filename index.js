88be11aa67b48db382d9fd0aa4cd04af235ef994
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-08-14 19:39:59 -0300

    criando repositorio

---
+++ index.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+
+const app = express()
+const PORT = 3000
+
+
+
+
+    //FUNC PARA FAZER SCRAPING DA PAGINA 
